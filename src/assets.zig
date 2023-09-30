716e972dd86272b5542a190c2bdf2e042ad429d3
Tobias Simetsreiter <tobias.simetsreiter@meliot.de>
2023-09-30 21:39:48 +0200

    use embedAsset

--- src/assets.zig
+++ src/assets.zig
@@ -11,26 +11,26 @@ pub inline fn embedAsset(p: []const u8) []const u8 {
 }
 
 pub const map = std.ComptimeStringMap(StaticResponse, [_]struct { []const u8, StaticResponse }{
-    .{ "/", .{ .body = @embedFile(assets_path++"/index.html") } },
-    .{ "/index.html", .{ .body = @embedFile(assets_path++"/index.html") } },
+    .{ "/", .{ .body = embedAsset("/index.html") } },
+    .{ "/index.html", .{ .body = embedAsset("/index.html") } },
     .{ "/index.js", .{
-        .body = @embedFile(assets_path++"/index.js"),
+        .body = embedAsset("/index.js"),
         .content_type = "text/javascript",
     } },
     .{ "/client.wasm", .{
-        .body = @embedFile(assets_path++"/client.wasm"),
+        .body = embedAsset("/client.wasm"),
         .content_type = "application/wasm",
     } },
     .{ "/xterm.min.js", .{
-        .body = @embedFile(assets_path++"/xterm.min.js"),
+        .body = embedAsset("/xterm.min.js"),
         .content_type = "text/javascript",
     } },
     .{ "/xterm.min.css", .{
-        .body = @embedFile(assets_path++"/xterm.min.css"),
+        .body = embedAsset("/xterm.min.css"),
         .content_type = "text/css",
     } },
     .{ "/xterm-addon-attach.min.js", .{
-        .body = @embedFile(assets_path++"/xterm-addon-attach.min.js"),
+        .body = embedAsset("/xterm-addon-attach.min.js"),
         .content_type = "text/javascript",
     } },
 });
