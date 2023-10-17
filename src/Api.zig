5a7942c8b5fcd5a3c1c4fac331589a37604822f9
Tobias Simetsreiter <tobias.simetsreiter@wabtec.com>
2023-10-17 15:23:45 +0200

    upgrade zbuild api context

--- src/Api.zig
+++ src/Api.zig
@@ -1,19 +1,23 @@
 const zap = @import("zap");
 const Websocket = @import("Websocket.zig");
 
-pub fn on_init(listener: zap.SimpleEndpointListener) void {
+pub fn init(listener: zap.SimpleEndpointListener) void {
     Websocket.GlobalContextManager =
         Websocket.ContextManager.init(
-            &[_][]const u8{ "ls", "-alsh" },
-            listener.allocator,
-            "wololo",
-            "derp-",
-        );
+        &[_][]const u8{ "ls", "-alsh" },
+        listener.allocator,
+        "wololo",
+        "derp-",
+    );
 }
 
-pub const on_upgrade = Websocket.on_upgrade;
+pub fn on_upgrade(ctx: *void, r: zap.SimpleRequest, proto: []const u8) void {
+    _ = ctx;
+    Websocket.on_upgrade(r, proto);
+}
 
-pub fn on_request(r: zap.SimpleRequest) void {
+pub fn on_request(ctx: *void, r: zap.SimpleRequest) void {
+    _ = ctx;
     r.setStatus(.not_found);
     r.sendBody("<html><body><h1>404 - Not Found</h1></body></html>") catch return;
 }
